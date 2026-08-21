c2ccf2abd51f76132eb719fce8f1b06219e16926
2lokeshrao <lokeshrao050@gmail.com>
2026-08-21 22:52:27 +0530

    Update

---
+++ get_thunderpick.js
@@ -0,0 +1,13 @@
+fetch('http://localhost:3000/api/admin/login', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ password: '@dmin123' })
+}).then(res => res.json()).then(async loginData => {
+  const token = loginData.token;
+  
+  const dataRes = await fetch('http://localhost:3000/api/admin/data', {
+    headers: { 'Authorization': `Bearer ${token}` }
+  });
+  const data = await dataRes.json();
+  console.log(JSON.stringify(data.platforms.find(p => p.id === 'thunderpick'), null, 2));
+}).catch(console.error);
